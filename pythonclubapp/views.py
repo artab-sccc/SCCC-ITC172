3bf55d46fddecf63e19e56061f7eba30a5992877
artab-sccc <64048501+artab-sccc@users.noreply.github.com>
2020-05-05 22:19:47 -0700

    assignment 4 and 5 complete
    files updated to reflect completion of assignment 4 and 5

--- pythonclubapp/views.py
+++ pythonclubapp/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import meeting, meetingminutes, resource, event
 
 # Create your views here.
 def index(request): 
     return render(request, 'pythonclubapp/index.html')
-    
+
+def getresources(request):
+    type_list=resource.objects.all()
+    return render(request, 'pythonclubapp/resources.html', {'type_list' : type_list})
+
+
