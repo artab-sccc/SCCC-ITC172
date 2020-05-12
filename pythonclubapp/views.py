33620105e1b5355ddde7bec7b45dbf0c59b40419
artab-sccc <64048501+artab-sccc@users.noreply.github.com>
2020-05-12 11:03:08 -0700

    Assignment 6 complete
    Assignment 6 complete

--- pythonclubapp/views.py
+++ pythonclubapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import meeting, meetingminutes, resource, event
 
 # Create your views here.
@@ -9,4 +9,15 @@ def getresources(request):
     type_list=resource.objects.all()
     return render(request, 'pythonclubapp/resources.html', {'type_list' : type_list})
 
+def getmeetings(request):
+    type_list=meeting.objects.all()
+    return render(request, 'pythonclubapp/meetings.html', {'type_list' : type_list})
 
+def meetingdetails(request, id):
+    meet=get_object_or_404(meeting, pk=id)
+    location=meet.meetinglocation
+    context={
+        'meet' : meet,
+        'location' : location,
+    }
+    return render(request, 'pythonclubapp/meetingdetails.html', context=context)
