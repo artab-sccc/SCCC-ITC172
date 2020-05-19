9306a8f13065eec12858ecbedf487c14cf5c0c05
artab-sccc <64048501+artab-sccc@users.noreply.github.com>
2020-05-19 12:13:59 -0700

    Assignment 7; tests complete
    Assignment 7; tests complete

--- pythonclubapp/tests.py
+++ pythonclubapp/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
+from .models import meeting, meetingminutes, resource, event
 
-# Create your tests here.
+# Tests the 'meeting' model
+class MeetingTitleTest(TestCase):
+    def test_string(self):
+        meet=meeting(meetingtitle="Meeting 1")
+        self.assertEqual(str(meet), meet.meetingtitle)
+    
+    def test_table(self):
+        self.assertEqual(str(meeting._meta.db_table), 'title')
+
+# Tests the 'meetingminutes' model
+class MeetingMinutesTest(TestCase):
+    def test_string(self):
+        meet=meetingminutes(minutestext="Minutes Text")
+        self.assertEqual(str(meet), meet.minutestext)
+    
+    def test_table(self):
+        self.assertEqual(str(meetingminutes._meta.db_table), 'meetingminutes')
+
+# Tests the 'resource' model
+class ResourceTest(TestCase):
+    def test_string(self):
+        res=resource(resourcename="Resource 1")
+        self.assertEqual(str(res), res.resourcename)
+    
+    def test_table(self):
+        self.assertEqual(str(resource._meta.db_table), 'resource')
+
+
+# Tests the 'event' model
+class EventTest(TestCase):
+    def test_string(self):
+        even=event(eventtitle="Event 1")
+        self.assertEqual(str(even), even.eventtitle)
+    
+    def test_table(self):
+        self.assertEqual(str(event._meta.db_table), 'event')
